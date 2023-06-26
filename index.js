2359bf604beebda1f8da7e06ae188d2e046c4d83
Jose Francisco Villalobos Diaz <francisco.77.fd@live.com>
2023-06-25 19:02:12 -0600

    quite la conexion con mysql

--- index.js
+++ index.js
@@ -5,22 +5,8 @@ const mysql = require("mysql2");
 const app = express();
 app.use(express.json());
 
-const HOST = process.env.MYSQLHOST || "localhost";
-const USER = process.env.MYSQLUSER || "root";
-const PASS = process.env.MYSQLPASSWORD || "root";
-const DB = process.env.MYSQLDATABASE || "railway";
-const PORT = process.env.PORT || 8889;
-
 app.use(bodyParse.urlencoded({extended: true}));
 app.use(bodyParse.json());
-
-const db = mysql.createConnection({
-  host:HOST,
-  user:USER,
-  password:PASS,
-  database:DB,
-  port: PORT
-});
 
 app.get("/", (req, res) => {
   // Envía la respuesta HTTP después de imprimir los resultados en la consola
