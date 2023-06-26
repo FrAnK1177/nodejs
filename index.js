9a4ffe204d8d9ba92556389a959644605fc75ada
Jose Francisco Villalobos Diaz <francisco.77.fd@live.com>
2023-06-26 10:56:27 -0600

    agregue las demas funciones del crud en el backend

--- index.js
+++ index.js
@@ -27,15 +27,61 @@ app.get("/books", (req,res)=>{
   const q = "select * from books";
   db.query(q,(err,data)=>{
       if(err) return res.json(err)
-      console.log('Query result:');
-      console.log(data);
+      //console.log('Query result:');
+      //console.log(data);
       return res.json(data)
   });
 });
 
 app.post("/books", (req,res)=>{
-  const {username} = req.body;
-  res.status(200).send({msg: `Hola, ${username}`});
+  const q = "insert into books (`title`, `descr`, `price`, `cover`) values (?)";
+  const values = [
+      req.body.title,
+      req.body.descr,
+      req.body.price,
+      req.body.cover,
+  ];
+
+  db.query(q,[values],(err,data)=>{
+      if(err) return res.json(err)
+      return res.json("Book has been added successfully.")
+  })
+});
+
+app.delete("/books/:id", (req,res)=>{
+  const bookId = req.params.id;
+  const q = "delete from books where id = ?";
+
+  db.query(q,[bookId],(err,data)=>{
+      if(err) return res.json(err)
+      return res.json("Book has ben deleted successfully.")
+  });
+});
+
+app.get("/books/:id", (req,res)=>{
+  const bookId = req.params.id;
+  const q = "select * from books where id = ?";
+  db.query(q,[bookId],(err,data)=>{
+      if(err) return res.json(err)
+      return res.json(data)
+  })
+})
+
+app.put("/books/:id", (req,res)=>{
+  const bookId = req.params.id;
+  const q = "update books set `title` = ?, `descr` = ?, `cover` = ?, `price` = ? where id = ?";
+
+  const values = [
+      req.body.title,
+      req.body.descr,
+      req.body.cover,
+      req.body.price,
+  ];
+
+  db.query(q,[...values, bookId],(err,data)=>{
+      if(err) return res.json(err)
+      return res.json("Book has ben updated successfully.")
+  });
 });
 
 app.listen(PORT, () =>{
